ddc78180dedcb85fd8fbd38a3a56a53d47a8c87f
JungUCheong <144769116+JungUCheong@users.noreply.github.com>
2025-02-18 22:43:41 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -37,10 +37,10 @@ def load_model():
             model=YOLO(pt_model_path)
             return model
         if onnx_model_path==None:
-            model=YOLO(pt_model_path, weights_only=True)
+            model=torch.load(pt_model_path, weights_only=True)
             return model
     if onnx_model_path!=None:
-        model=YOLO(onnx_model_path, weights_only=True)
+        model=torch.load(onnx_model_path, weights_only=True)
         return model
 def infer_image(model,image,conf,iou,is_save=False):
     '''
